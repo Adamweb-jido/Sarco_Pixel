ca9878ebefc65bc4cf4fb6db2e8c30c47532445f
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-08-29 16:42:19 +0100

    Sarco Pixel First Commit

--- app/src/main/java/com/adamweb/sarcopixel/LoginActivity.java
+++ app/src/main/java/com/adamweb/sarcopixel/LoginActivity.java
@@ -2,7 +2,10 @@ package com.adamweb.sarcopixel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView textView = (TextView) findViewById(R.id.signupText);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/adamweb/sarcopixel/RegisterPage.java
+++ app/src/main/java/com/adamweb/sarcopixel/RegisterPage.java
@@ -1,14 +1,29 @@
 package com.adamweb.sarcopixel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterPage extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        TextView loginPage = (TextView) findViewById(R.id.loginTextView);
+        loginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

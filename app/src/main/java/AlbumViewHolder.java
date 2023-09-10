c22ecfb2f731670566f0ccf022a7b8f624467bf6
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-10 12:34:22 +0100

    another commit

---
+++ app/src/main/java/AlbumViewHolder.java
@@ -0,0 +1,17 @@
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AlbumViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView imageView1, imageView2, imageView3;
+    TextView textView1, textView2;
+
+    public AlbumViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView1 = itemView.findViewById(R.id.)
+    }
+}

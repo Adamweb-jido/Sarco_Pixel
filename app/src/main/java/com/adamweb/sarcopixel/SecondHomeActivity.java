ef563e4f077e76e68cc231c086373629bfc622a2
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-10 16:16:38 +0100

    Another commit

--- app/src/main/java/com/adamweb/sarcopixel/SecondHomeActivity.java
+++ app/src/main/java/com/adamweb/sarcopixel/SecondHomeActivity.java
@@ -18,10 +18,8 @@ public class SecondHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second_home);
 
         List<AlbumItem> items = new ArrayList<>();
-        items.add(new AlbumItem("Adam's Album", "01-01-2023", R.drawable.albumimage1, R.drawable.albumimage2, R.drawable.albumimage3));
-        items.add(new AlbumItem("Adam's Album", "01-01-2023", R.drawable.albumimage1, R.drawable.albumimage2, R.drawable.albumimage3));
-        items.add(new AlbumItem("Adam's Album", "01-01-2023", R.drawable.albumimage1, R.drawable.albumimage2, R.drawable.albumimage3));
-        items.add(new AlbumItem("Adam's Album", "01-01-2023", R.drawable.albumimage1, R.drawable.albumimage2, R.drawable.albumimage3));
+        items.add(new AlbumItem("Adam's Album", "01-01-2023",R.drawable.aicon, R.drawable.albumimage2, R.drawable.albumimage3));
+
         recyclerView = findViewById(R.id.albumRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new AlbumAdapter(getApplicationContext(), items));
